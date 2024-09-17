f68226ff2bad8b5018cc795bca3bd245997a6cc8
Hemant Kumar Gupta <45016990+happygupta@users.noreply.github.com>
2024-09-17 17:03:50 +0530

    web app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3009628943"
+    serviceWorkerVersion: "8557970"
   }
 });
